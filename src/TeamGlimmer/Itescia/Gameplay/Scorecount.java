406fce7847bcbbd5e7c74fdccdc4c29e323a54ad
Nicolas Maurice <nicolas.maurice@edu.itescia.fr>
2020-10-22 15:28:29 +0200

    added changes to backup

--- src/TeamGlimmer/Itescia/Gameplay/Scorecount.java
+++ src/TeamGlimmer/Itescia/Gameplay/Scorecount.java
@@ -1,2 +1,18 @@
-package TeamGlimmer.Itescia.Gameplay;public class Scorecount {
+package TeamGlimmer.Itescia.Gameplay;
+
+public class Scorecount {
+
+    static int scoreCurrent[]={0};
+
+    public static void increment() {
+        scoreCurrent[0]+=1;
+
+
+    }
+
+
+    public static void displayScore(){
+        System.out.println("My score:"+ scoreCurrent);
+    }
+
 }
